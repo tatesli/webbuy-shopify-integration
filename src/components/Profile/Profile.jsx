86ff115124e14a652cb4315db6e6116c173feb98
Tetiana Slidenko <139694109+tatesli@users.noreply.github.com>
2025-05-06 20:17:19 +0200

    Dyplom fixes part three
    fix: fix eslint for netlify deploy, remove unused imports, minor styles fix

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,10 +1,11 @@
 import React from "react";
-import { Link, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { useSnackbar } from "notistack";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faClose } from "@fortawesome/free-solid-svg-icons";
 
+import { ROUTES } from "../../pages/Routes/Routes";
+import { clearCart } from "../../features/cart/cartSlice";
+import { getUser } from "../../features/user/userSlice";
 import { ROUTES } from "../../pages/Routes/Routes";
 import { clearCart } from "../../features/cart/cartSlice";
 import { getUser } from "../../features/user/userSlice";
@@ -14,9 +15,17 @@ import { Button, ButtonType } from "../Button";
 import { UserForm } from "../User";
 import styles from "./Profile.module.css";
 import { Modal } from "../Modal";
+import { Button, ButtonType } from "../Button";
+import { UserForm } from "../User";
+import styles from "./Profile.module.css";
+import { Modal } from "../Modal";
 
 export const Profile = ({ onClose, isOpen }) => {
+export const Profile = ({ onClose, isOpen }) => {
   const dispatch = useDispatch();
+  const user = useSelector(getUser);
+  const navigate = useNavigate();
+  const { enqueueSnackbar } = useSnackbar();
   const user = useSelector(getUser);
   const navigate = useNavigate();
   const { enqueueSnackbar } = useSnackbar();
@@ -24,6 +33,10 @@ export const Profile = ({ onClose, isOpen }) => {
   const handleLogout = () => {
     dispatch(logoutUser());
     dispatch(clearCart());
+    enqueueSnackbar("You have successfully logged out!", {
+      variant: "success",
+    });
+    onClose();
     enqueueSnackbar("You have successfully logged out!", {
       variant: "success",
     });
@@ -38,12 +51,40 @@ export const Profile = ({ onClose, isOpen }) => {
     onClose();
   };
 
+  if (!user) {
+    return null;
+  }
+  const handleLinkClick = (path) => {
+    navigate(path);
+    onClose();
+  };
+
   return (
+    <Modal isOpen={isOpen} onClose={onClose} title="Profile">
     <Modal isOpen={isOpen} onClose={onClose} title="Profile">
       {user ? (
         <>
           <h1 className={styles.title}>Welcome, {user.user?.name}!</h1>
+          <h1 className={styles.title}>Welcome, {user.user?.name}!</h1>
           <div className={styles.links}>
+            <Button
+              type={ButtonType.default}
+              onClick={() => handleLinkClick(ROUTES.CART)}
+            >
+              Your Cart
+            </Button>
+            <Button
+              type={ButtonType.default}
+              onClick={() => handleLinkClick(ROUTES.FAVORITES)}
+            >
+              Your Favorites
+            </Button>
+            <Button
+              type={ButtonType.default}
+              onClick={() => handleLinkClick(ROUTES.ORDERS)}
+            >
+              Your Orders
+            </Button>
             <Button
               type={ButtonType.default}
               onClick={() => handleLinkClick(ROUTES.CART)}
@@ -70,11 +111,18 @@ export const Profile = ({ onClose, isOpen }) => {
               label="Logout"
               fullWidth
             />
+            <Button
+              type={ButtonType.primary}
+              onClick={handleLogout}
+              label="Logout"
+              fullWidth
+            />
           </div>
         </>
       ) : (
         <UserForm />
       )}
+    </Modal>
     </Modal>
   );
 };
